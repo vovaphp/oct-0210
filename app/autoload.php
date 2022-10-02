0a882596eede9224a748c02e302f154a3013d41a
vovaphp <sahno023@gmail.com>
2022-10-02 23:18:40 +0300

    index.php autoload.php

---
+++ app/autoload.php
@@ -0,0 +1,9 @@
+<?php
+spl_autoload_register(function ($className) {
+    $classFile = 'app/' . $className . '.php';
+    if (file_exists($classFile)) {
+        include_once $classFile;
+        return true;
+    }
+    return false;
+});
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+include_once 'app/autoload.php';
+\core\Route::init();
